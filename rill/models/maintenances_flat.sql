656ca707480d5dce92302e68b46b9f458655ac77
adriens <adriens@users.noreply.github.com>
2026-06-03 04:40:08 +1100

    chore(rill): alignement des modèles Rill avec les nouvelles colonnes DuckDB

--- rill/models/maintenances_flat.sql
+++ rill/models/maintenances_flat.sql
@@ -1,5 +1,9 @@
 SELECT 
     m.*,
+    CASE 
+        WHEN hour(m.timestamp_debut) BETWEEN 6 AND 18 THEN 'JOUR (06h-18h)'
+        ELSE 'NUIT (18h-06h)'
+    END as periode_jour_nuit,
     c.commune,
     s.service,
     g.lat,
